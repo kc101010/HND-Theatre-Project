17281a9c2502900eed6304aba2677b71d9bacaa8
kc101010 <blueboi.kc@gmail.com>
2021-03-16 13:39:24 +0000

    first

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*http://192.168.10.10/AssessmentProject/index.php*/
+
+//https://placeholder.com/
+//https://fontawesome.com/
+//https://unsplash.com/
+//https://pixabay.com/
+
+echo("<h1> Hello!! </h1>");
+
