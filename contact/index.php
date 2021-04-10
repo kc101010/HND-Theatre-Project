7cc9ba4adbc8c65a3f398165fcb772e2c2b07b96
kc101010 <blueboi.kc@gmail.com>
2021-04-10 16:49:38 +0100

    mobile design, fixes

--- contact/index.php
+++ contact/index.php
@@ -3,7 +3,7 @@
 <section id="sect_main">
     <h1 id="main_header"> Contact us </h1>
 
-    <p class="con_text">Phone Number: 01505 000000 </p>
+    <p class="con_text"> Phone Number: 01505 000000 </p>
     <p class="con_text"> <a target="_blank" href="mailto: email@localtheatre.com" > Email: email@localtheatre.com </a> </p>
     <p class="con_text"> <a> Facebook </a> </p>
     <p class="con_text"> <a> Twitter </a> </p>
--- partial/header.php
+++ partial/header.php
@@ -9,18 +9,26 @@
     <link rel="stylesheet" href="../css/nav.css" type="text/css"/>
     <link rel="stylesheet" href="../css/footer.css" type="text/css"/>
     <link rel="stylesheet" href="../css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="../css/mobile.css" type="text/css"/>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Monda:wght@700&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/abc0c36e7b.js" crossorigin="anonymous"></script>
 
     <title>Local Theatre Company</title>
 
 </head>
 
 
-<body id="hdr-nav-container">
+<body>
 
-    <section id="sec_hdr-nav">
+    <section id="hdr-nav-container">
+
+        <span id="mob-nav-bar">
+            <i class="fas fa-bars"></i>
+        </span>
+
         <h1 id="hdr-text"> <a href="../home/index.php"> Local Theatre Company </a> </h1>
 
         <nav id="hdr-nav">
