a2413ba380b5e268bc4caa1dcc7fb29f22f7ac74
kc101010 <blueboi.kc@gmail.com>
2021-04-19 13:30:15 +0100

    starting blog

--- blog/index.php
+++ blog/index.php
@@ -3,8 +3,25 @@
     <section id="sect_main">
         <h1 id="main_header"> Blog </h1>
 
+    </section>
 
+    <section id="sect_blog">
+        <div id="blog_cat">
+            <h2 id="blog_cat_head"> Categories </h2>
 
+            <ul id="blog_cat_list">
+                <li>Announcements</li>
+                <li>Articles</li>
+                <li>Our Reviews</li>
+            </ul>
+
+        </div>
+
+        <div id="latest_article">
+            <h2> Latest Article</h2>
+
+            <p> A small replacement placeholder description</p>
+        </div>
     </section>
 
 <?php include_once "../partial/footer.php";?>
