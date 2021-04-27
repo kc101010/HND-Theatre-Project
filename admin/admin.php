7770bd10a706c92d03860bce331733542c7eb2f2
kc101010 <blueboi.kc@gmail.com>
2021-04-27 14:25:30 +0100

    starting adming/login pages

---
+++ admin/admin.php
@@ -0,0 +1,16 @@
+<?php include_once "../partial/header.php";?>
+    <section id="sect_main">
+        <h1 id="main_header"> Dashboard [Admin] </h1>
+
+        <p class="main_text"> username: </p>
+        <p class="main_text"> email: </p>
+
+    </section>
+    <hr id="admin_panel_sep">
+    <section id="sect_admin_panel">
+        <h1 id="main_header"> Manage Users </h1>
+
+        <p class="main_text"> De-activate user</p>
+    </section>
+
+<?php include_once "../partial/footer.php";?>
---
+++ user/user.php
@@ -0,0 +1,12 @@
+<?php include_once "../partial/header.php";?>
+<section id="sect_main">
+    <h1 id="main_header"> Dashboard </h1>
+
+    <p class="main_text"> username: </p>
+    <p class="main_text"> email: </p>
+
+
+
+</section>
+
+<?php include_once "../partial/footer.php";?>
