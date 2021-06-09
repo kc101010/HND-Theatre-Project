71a884e7090bd437a2579405b121a0bef5ed6b32
kc101010 <blueboi.kc@gmail.com>
2021-06-09 17:28:04 +0100

    commenting for conn.php

--- connection/conn.php
+++ connection/conn.php
@@ -1,12 +1,14 @@
 <?php
+//WRITTEN BY KYLE CHRISTIE, GLASGOW CLYDE COLLEGE (2021) -- DEVELOPING WEBSITES FOR MULTIPLATFORM USE ~ HND Software Development
 
+//start a session, stores login and similar data
 session_start();
 
-//connection info
+//declares connection info
 $host = 'localhost';
 $usern = 'homestead';
 $pass = 'secret';
 $database = 'TheatreCompanyProject';
 
-//attempt a connection
+//attempt connection to DB and handle errors
 if(mysqli_connect_errno()){ exit('Failed to connect to MySQL: ' . mysqli_connect_error());}
