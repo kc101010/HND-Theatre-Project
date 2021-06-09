99547515747824077a3f67e5f632cc1d4763bbf4
Kyle Christie <55036011+kc101010@users.noreply.github.com>
2021-06-09 17:53:43 +0100

    Delete account directory

--- account/management.php
+++
@@ -1,9 +0,0 @@
-<form id="change_email">
-    <input class="form_component" type="email" name="change_user_email" placeholder="Email">
-    <input id="frm_submit" class="form_component" type="submit" value="Change Email">
-</form>
-
-<form id="change_password">
-    <input class="form_component" type="password" name="change_user_password" placeholder="New Password">
-    <input id="frm_submit" class="form_component" type="submit" value="Change password">
-</form>
